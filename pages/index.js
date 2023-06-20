007e38397c16967c3c39b5a400f1ea202fae01da
Danilo Setin <34036386+danilovsetin@users.noreply.github.com>
2023-06-20 13:37:13 +0000

    alteração mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Seja Bem Vindo!</h1>
+    return <h1>Se você é curioso(a) <a href="https://i.pinimg.com/originals/d3/3c/10/d33c107a41227c45b15e5d1b285f6277.jpg" target="_blank">Clique aqui</a></h1>
 }
 
 export default Home

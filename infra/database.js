0c907ac9c79e49ee190e5af09db111c6f2de694c
Danilo Setin <34036386+danilovsetin@users.noreply.github.com>
2024-02-26 22:13:20 +0000

    makes `database.js` more robust to logger

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,21 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
+
+  console.log("Credenciais DB", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }

9477ac034c884532c03a5eade6e63919f9078060
Danilo Setin <34036386+danilovsetin@users.noreply.github.com>
2024-11-22 19:36:29 +0000

    add workflow

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -18,14 +18,10 @@ test("GET to /api/v1/status should return 200", async () => {
 
   //version
   expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(typeof responseBody.dependencies.database.version).toBe("string");
-  expect(responseBody.dependencies.database.version.length).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.version).toBe("20.0");
 
   //max_connections
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number"
-  );
   expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
   expect(
     responseBody.dependencies.database.max_connections
@@ -35,9 +31,6 @@ test("GET to /api/v1/status should return 200", async () => {
 
   //current_connections
   expect(responseBody.dependencies.database.current_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.current_connections).toBe(
-    "number"
-  );
   expect(
     responseBody.dependencies.database.current_connections
   ).toBeLessThanOrEqual(responseBody.dependencies.database.max_connections);

aa9d5047cdb6137b066d5f77d834c77082bf4de6
Danilo Setin <34036386+danilovsetin@users.noreply.github.com>
2026-06-01 16:28:27 +0000

    feat: has password in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
+import password from "models/password.js";
 
 async function findOneByUsername(username) {
   const result = await database.query({
@@ -29,6 +30,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   var newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -73,6 +75,11 @@ async function create(userInputValues) {
         action: "Utilize outro nome de usuário para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
